2e6bf43efe91e5ec1ddfb117c2216cce2ff23943
datvm <datvm@mageplaza.com>
2019-06-13 15:52:29 +0700

    [wip] compatible with Magento 2.3.x

--- Observer/ProcessCronQueueObserver.php
+++ Observer/ProcessCronQueueObserver.php
@@ -21,15 +21,6 @@
 
 namespace Mageplaza\CronSchedule\Observer;
 
-use Magento\Cron\Model\ConfigInterface;
-use Magento\Cron\Model\ScheduleFactory;
-use Magento\Framework\App\CacheInterface;
-use Magento\Framework\App\Config\ScopeConfigInterface;
-use Magento\Framework\App\Console\Request;
-use Magento\Framework\ObjectManagerInterface;
-use Magento\Framework\Process\PhpExecutableFinderFactory;
-use Magento\Framework\ShellInterface;
-use Magento\Framework\Stdlib\DateTime\TimezoneInterface;
 use Mageplaza\CronSchedule\Helper\Data;
 
 /**
@@ -38,52 +29,6 @@ use Mageplaza\CronSchedule\Helper\Data;
  */
 class ProcessCronQueueObserver extends \Magento\Cron\Observer\ProcessCronQueueObserver
 {
-    /**
-     * @var Data
-     */
-    private $helper;
-
-    /**
-     * ProcessCronQueueObserver constructor.
-     *
-     * @param ObjectManagerInterface $objectManager
-     * @param ScheduleFactory $scheduleFactory
-     * @param CacheInterface $cache
-     * @param ConfigInterface $config
-     * @param ScopeConfigInterface $scopeConfig
-     * @param Request $request
-     * @param ShellInterface $shell
-     * @param TimezoneInterface $timezone
-     * @param PhpExecutableFinderFactory $phpExecutableFinderFactory
-     * @param Data $helper
-     */
-    public function __construct(
-        ObjectManagerInterface $objectManager,
-        ScheduleFactory $scheduleFactory,
-        CacheInterface $cache,
-        ConfigInterface $config,
-        ScopeConfigInterface $scopeConfig,
-        Request $request,
-        ShellInterface $shell,
-        TimezoneInterface $timezone,
-        PhpExecutableFinderFactory $phpExecutableFinderFactory,
-        Data $helper
-    ) {
-        $this->helper = $helper;
-
-        parent::__construct(
-            $objectManager,
-            $scheduleFactory,
-            $cache,
-            $config,
-            $scopeConfig,
-            $request,
-            $shell,
-            $timezone,
-            $phpExecutableFinderFactory
-        );
-    }
-
     /**
      * @param string $jobCode
      * @param string $cronExpression
@@ -94,7 +39,8 @@ class ProcessCronQueueObserver extends \Magento\Cron\Observer\ProcessCronQueueOb
      */
     protected function saveSchedule($jobCode, $cronExpression, $timeInterval, $exists)
     {
-        if ($this->helper->isJobDisabled($jobCode)) {
+        // compatible with multiple Magento version
+        if ($this->_objectManager->get(Data::class)->isJobDisabled($jobCode)) {
             return;
         }
 
